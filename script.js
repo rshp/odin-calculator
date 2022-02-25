6b172bf3b2d320abaac3cc294787ff2fe3e54d42
shpr <roman.shpr@gmail.com>
2022-02-25 22:01:23 +0200

    Fix regex ignoring division
    Error found in filtering function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function parseWithRegex(string) {
-	let regEx = /([0-9]*[.])?[0-9]+|\(|\)|\+|-|\*|\^/g;
+	let regEx = /([0-9]*[.])?[0-9]+|\(|\)|\+|-|\*|\^|\//g;
 	return string.match(regEx);
 }
 
@@ -18,7 +18,7 @@ function parseNumbers(arry) {
 function countParens(arry) {
 	let parensIndicator = [];
 	let parensCount = 0;
-	parsed.forEach((element, index) => {
+	arry.forEach((element, index) => {
 		if (element == '(') {
 			parensCount += 1;
 			parensIndicator[index] = parensCount;
@@ -33,19 +33,17 @@ function countParens(arry) {
 function calcHighestParens(arry, parens) {
 	let maxParensValue = Math.max(...parens);
 	let maxParensStartIndex = parens.indexOf(maxParensValue);
+	//let endOfSequence = 0;
 	let filtered = arry.filter((value, index) => {
-		return parens[index] == maxParensValue;
+		//here is the problem where filtered should be only the first encoutered UNINTERRUPTED sequence of maxParensValue.
+		if (parens[index] == maxParensValue) return true; //this should be rewritten so that any other segments of maxParensValue in array are ignored
 	});
-	let evaluated = 'evaluated'; //calc filtered array here
-	arry.splice(maxParensStartIndex, filtered.length, evaluated);
+	console.log(filtered);
+	let spliceLength = filtered.length;
+	let evaluated = evaluateFiltered(filtered); //calc filtered array here
+	arry.splice(maxParensStartIndex, spliceLength, evaluated);
 	return arry;
 }
-
-let testString = '0.21^(87+36)/10-23^2*(2.9-1.58)^.5+(((.98-48)*3.77)^2.9)-3.9';
-let parsedWithRegex = parseWithRegex(testString);
-let parsed = parseNumbers(parsedWithRegex);
-let parens = countParens(parsed);
-calcHighestParens(parsed, parens);
 
 function evaluateFiltered(expression) {
 	if (expression[0] === '(') {
@@ -64,6 +62,11 @@ function evaluateFiltered(expression) {
 		localRes = expression[indx - 1] * expression[indx + 1];
 		expression.splice(indx - 1, 3, localRes);
 	}
+	while (expression.indexOf('/') !== -1) {
+		indx = expression.indexOf('/');
+		localRes = expression[indx - 1] / expression[indx + 1];
+		expression.splice(indx - 1, 3, localRes);
+	}
 	while (expression.indexOf('-') !== -1) {
 		indx = expression.indexOf('-');
 		localRes = expression[indx - 1] - expression[indx + 1];
@@ -75,6 +78,20 @@ function evaluateFiltered(expression) {
 		expression.splice(indx - 1, 3, localRes);
 	}
 	return +expression; //get rid of single member array
+}
+
+let testString = '0.21^(87+36)/10-23^2*(2.9-1.58)^.5+(((.98-48)*3.77)^2.9)-3.9';
+let parsedWithRegex = parseWithRegex(testString);
+let parsed = parseNumbers(parsedWithRegex);
+evaluateArray(parsed);
+
+function evaluateArray(arry) {
+	let parens = countParens(arry);
+	console.table([arry, parens]);
+	let calculated = calcHighestParens(arry, parens);
+	while (arry.length > 1) {
+		evaluateArray(calculated);
+	}
 }
 
 //given an array1

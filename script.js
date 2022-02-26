cf0158fb2680f8a77868aa8dca7a619f903c56f6
shpr <roman.shpr@gmail.com>
2022-02-26 16:05:49 +0200

    Finish parser logic
    Merge preparsing functions

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-function parseWithRegex(string) {
+function expressionToArray(string) {
 	let regEx = /([0-9]*[.])?[0-9]+|\(|\)|\+|-|\*|\^|\//g;
-	return string.match(regEx);
-}
-
-//Convert number strings to numbers
-function parseNumbers(arry) {
-	let output = [...arry]; //clone arry
-	arry.forEach((element, index) => {
+	let regexParsed = string.match(regEx);
+	let output = [...regexParsed]; //clone arry
+	regexParsed.forEach((element, index) => {
 		if (!isNaN(+element)) {
-			output[index] = +element;
+			output[index] = +element; //convert number strings to numbers
 		}
 	});
 	return output;
@@ -33,12 +29,11 @@ function countParens(arry) {
 function calcHighestParens(arry, parens) {
 	let maxParensValue = Math.max(...parens);
 	let maxParensStartIndex = parens.indexOf(maxParensValue);
-	//let endOfSequence = 0;
-	let filtered = arry.filter((value, index) => {
-		//here is the problem where filtered should be only the first encoutered UNINTERRUPTED sequence of maxParensValue.
-		if (parens[index] == maxParensValue) return true; //this should be rewritten so that any other segments of maxParensValue in array are ignored
-	});
-	console.log(filtered);
+	let filtered = [];
+	for (let i = maxParensStartIndex; i < arry.length; i++) {
+		if (parens[i] == maxParensValue) filtered.push(arry[i]);
+		else break;
+	}
 	let spliceLength = filtered.length;
 	let evaluated = evaluateFiltered(filtered); //calc filtered array here
 	arry.splice(maxParensStartIndex, spliceLength, evaluated);
@@ -80,24 +75,15 @@ function evaluateFiltered(expression) {
 	return +expression; //get rid of single member array
 }
 
-let testString = '0.21^(87+36)/10-23^2*(2.9-1.58)^.5+(((.98-48)*3.77)^2.9)-3.9';
-let parsedWithRegex = parseWithRegex(testString);
-let parsed = parseNumbers(parsedWithRegex);
-evaluateArray(parsed);
-
 function evaluateArray(arry) {
 	let parens = countParens(arry);
-	console.table([arry, parens]);
 	let calculated = calcHighestParens(arry, parens);
 	while (arry.length > 1) {
 		evaluateArray(calculated);
 	}
+	return +calculated;
 }
 
-//given an array1
-//-calc array of parens
-//-get highest parens
-//-calc hiest parens term
-//-substitute term in array1
-//-call self with array1 until return length is 0
-//
+let testString = '0.21^(87+36)/10-23^2*(2.9-1.58)^.5+(((.98+48)*3.77)^2.9)-3.9';
+let parsed = expressionToArray(testString);
+console.log(evaluateArray(parsed));

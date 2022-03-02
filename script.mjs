e2aa7aa053101147ff51e8c61aaefc4e205e5bc8
shpr <roman.shpr@gmail.com>
2022-03-02 13:53:06 +0200

    Commit from work

--- script.mjs
+++ script.mjs
@@ -1,5 +1,20 @@
 import { CalculatorCounstructor } from './calculator.mjs';
 const calculator = new CalculatorCounstructor();
+
+const calculatorKeyboard = document.querySelector('.calculator-keyboard');
+calculatorKeyboard.addEventListener('click', (e) => {
+	console.log(e);
+	switch (e) {
+		case '(':
+			break;
+
+		default:
+			break;
+	}
+});
+
+console.log(calculatorKeyboard);
+
 calculator.pushDigit(7);
 //calculator.pushDigit(0);
 //calculator.pushDigit(0);
@@ -12,5 +27,5 @@ calculator.pushDigit(0);
 calculator.pushBackspace();
 calculator.pushEqual();
 
-console.log(calculator.expressionWindow);
-console.log(calculator.resultWindow);
+// console.log(calculator.expressionWindow);
+// console.log(calculator.resultWindow);
